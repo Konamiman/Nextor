b0a9891c424745fef4325de35602402e10f63654
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-04-07 17:25:31 +0200

    - Version stepped up to 2.0.2. - Change from v2.0 beta 2 to v2.0 final (changing the location of DVB_TABLE) rolled back, it was preventing Nextor from properly booting in DOS 1 mode. - Some errata in the getting started guide corrected.

--- source/msxdos25/compile.bat
+++ source/msxdos25/compile.bat
@@ -177,8 +177,8 @@ dd if=bank5\fdisk.dat of=dos250ba.dat bs=1 count=16000 seek=82176
 dd if=bank5\fdisk2.dat of=dos250ba.dat bs=1 count=8000 seek=98560
 copy bank4\b4rd.bin
 dd if=b4rd.bin of=dos250ba.dat bs=1 count=15 seek=65664
-copy dos250ba.dat Nextor-2.0.1.base.dat
-copy dos250ba.dat ..\..\bin\kernels\Nextor-2.0.1.base.dat
+copy dos250ba.dat Nextor-2.0.2.base.dat
+copy dos250ba.dat ..\..\bin\kernels\Nextor-2.0.2.base.dat
 
 echo .
 echo *****************
@@ -212,8 +212,8 @@ cpm32 L80 /P:7fd0,CHGBNK,CHGBNK/N/X/Y/E
 hex2bin -s 4000 driver.hex
 hex2bin -s 7FD0 CHGBNK.hex
 
-..\..\mknexrom  ..\..\Nextor-2.0.1.base.dat Nextor-2.0.1.%%~nc.ROM /d:driver.bin /m:chgbnk.bin
-copy Nextor-2.0.1.%%~nc.ROM ..\..\..\..\bin\kernels
+..\..\mknexrom  ..\..\Nextor-2.0.2.base.dat Nextor-2.0.2.%%~nc.ROM /d:driver.bin /m:chgbnk.bin
+copy Nextor-2.0.2.%%~nc.ROM ..\..\..\..\bin\kernels
 del b6.mac
 del *.rom
 cd ..
@@ -227,10 +227,10 @@ echo ***
 echo .
 
 cd sdscc
-..\..\mknexrom ..\..\Nextor-2.0.1.base.dat nextor2.rom /d:driver.bin /m:Mapper.ASCII8.bin
-copy nextor2.rom ..\..\..\..\bin\kernels\Nextor-2.0.1.MegaFlashSDSCC.ROM
+..\..\mknexrom ..\..\Nextor-2.0.2.base.dat nextor2.rom /d:driver.bin /m:Mapper.ASCII8.bin
+copy nextor2.rom ..\..\..\..\bin\kernels\Nextor-2.0.2.MegaFlashSDSCC.ROM
 sjasm makerecoverykernel.asm kernel.dat
-copy kernel.dat ..\..\..\..\bin\kernels\Nextor-2.0.1.MegaFlashSDSCC.Recovery.ROM
+copy kernel.dat ..\..\..\..\bin\kernels\Nextor-2.0.2.MegaFlashSDSCC.Recovery.ROM
 cd ..
 
 echo .
