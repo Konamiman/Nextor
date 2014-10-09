fd1e7e45b910e73ecaad21df4706986ad2fc0596
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2014-10-09 14:29:38 +0200

    NEXTOR.SYS did not contain messages in Japanese for abort/retry/ignore, reading/writing, etc. Now to separate files are created, one without Japanese messages (Enlgish is used instead) and one with (but it's bigger and takes more TPA space).

--- source/command25/msxdos/compile.bat
+++ source/command25/msxdos/compile.bat
@@ -4,15 +4,24 @@ cls
 copy ..\..\msxdos25\*.inc
 copy ..\..\msxdos25\codes.mac
 copy ..\..\msxdos25\data.mac
-for %%A in (CODES,DATA,KMSG,MESSAGES,REAL,REF,RELOC,SYS,VER,END) do cpm32 M80 =%%A
+for %%A in (CODES,DATA,KMSG,MESSAGES,REAL,REF,RELOC,VER,END) do cpm32 M80 =%%A
+copy NOKMSG.MAC USEKMSG.MAC
+cpm32 M80 =SYS
 cpm32 l80 /P:100,CODES,DATA,RELOC,VER,REF,SYS,REAL,SYS,MESSAGES,END,NEXTOR/n/x/y/e
+copy YESKMSG.MAC USEKMSG.MAC
+cpm32 M80 =SYS
+cpm32 l80 /P:100,CODES,DATA,RELOC,VER,REF,SYS,REAL,SYS,MESSAGES,KMSG,END,NEXTORK/n/x/y/e
+del USEKMSG.MAC
 hex2bin -s 100 nextor.hex
+hex2bin -s 100 nextork.hex
 del nextor.sys
+del nextork.sys
 ren nextor.bin NEXTOR.SYS
+ren nextork.bin NEXTORK.SYS
 del *.bin
 del *.hex
 del *.rel
 del *.sym
 if not exist ..\..\..\bin\tools md ..\..\..\bin\tools
 copy NEXTOR.SYS ..\..\..\bin\tools
-
+copy NEXTORK.SYS ..\..\..\bin\tools
