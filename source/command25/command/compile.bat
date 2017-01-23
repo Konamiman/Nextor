5117f2516190c88077ecbe306de6ef9ef306bec2
Néstor Soriano Vilchez <konamiman@stackoverflow.com>
2017-01-23 22:43:26 +0100

    Added compile.bat in the directory of command2.com

---
+++ source/command25/command/compile.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+copy ..\..\msxdos25\*.inc
+copy ..\..\msxdos25\codes.mac
+copy ..\..\msxdos25\data.mac
+for %%A in (CODES,DATA,START,CLI,CMD,COPY,DIRS,FILES,IO,JTEXT,MESSAGES,MISC,VAR,VER) do cpm32 M80 =%%A
+cpm32 l80 /P:100,CODES,DATA,START,CLI,CMD,COPY,DIRS,FILES,IO,JTEXT,MESSAGES,MISC,VAR,VER,COMMAND2/n/x/y/e
+hex2bin -s 100 command2.hex
+del command2.com
+ren command2.bin COMMAND2.COM
+del *.bin
+del *.hex
+del *.rel
+del *.sym
+if not exist ..\..\..\bin\tools md ..\..\..\bin\tools
+copy COMMAND2.COM ..\..\..\bin\tools
